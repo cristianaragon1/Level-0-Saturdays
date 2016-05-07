d044606d05971c315b66087d5faabef6aa9641dd
Love4653 <Kaiya.park@gmail.com>
2016-05-07 10:03:04 -0700

    bieber
    finisherd

---
+++ src/beiberFever.java
@@ -0,0 +1,30 @@
+
+public class beiberFever {
+public static void main(String[] args) {
+	for (int j = 0; j < 2; j++) {
+		
+	
+	for (int i = 0; i < 2; i++) {
+		chor();
+		System.out.println("oh");
+		chortwo();
+	}
+	System.out.println("I thought you'd always be mine");
+
+}
+}	
+public static void chor() {
+	for (int i = 0; i < 3; i++) {
+		System.out.println("Baby");
+	}
+}
+public static void chortwo(){
+	chor();
+	System.out.println("no");
+	
+}
+{
+	
+}
+}
+
