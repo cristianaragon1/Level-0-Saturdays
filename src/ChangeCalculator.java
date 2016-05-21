0ce7a8d26a18f0bf90545e634c496a90fe981b33
Love4653 <Kaiya.park@gmail.com>
2016-05-21 09:58:50 -0700

    calculator is finished
    calc fin

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -8,22 +8,53 @@ import javax.swing.JOptionPane;
  * make me a sandwich.
  */
 public class ChangeCalculator {
+		
+	
 
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-		String nicks = JOptionPane.showInputDialog("How many nickels do you have?");
+		
 		// Convert their answer to an int using Integer.parseInt()
-		Integer.parseInt(nicks);
 		// Ask the user how many dimes they have, and convert their answer
-		String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
-		Integer.parseInt(dimes);
+		
 
 		// Ask the user how many quarters they have, and convert their answer
-String quarters = JOptionPane.showInputDialog("How many quarters do you have?");
-		// Calculate how much money the user has and save it in a double variable
-String sum = nicks + dimes + quarters;
+		
+		// Calculate how much money the user has and save it in a double
+		// variable
+		//int nicksTotal = nicksNum * 5;
+		//int dimesTotal = dimesNum * 10; 
+		//int quartersTotal = quartersNum * 25;
+		//int realSum = Integer.parseInt(sum);
 		// Tell the user how much money they have
-JOptionPane.showMessageDialog(null, "You have" + sum);
+		int nickelsTotal = getNickelsAmount();
+		int dimesTotal = getDimesAmount();
+		int quartersTotal = getQuartersAmount();
+		int sum = nickelsTotal + dimesTotal + quartersTotal;
+
+		JOptionPane.showMessageDialog(null, "You have " + sum + " cents");
+	}
+
+	public static int getNickelsAmount() {
+		// TODO Auto-generated method stub
+		String nicks = JOptionPane.showInputDialog("How many nickels do you have?");
+		int nicksNum = Integer.parseInt(nicks);
+		int nicksTotal = nicksNum *5;
+		return nicksTotal;
+	}
+	public static int getDimesAmount() {
+		// TODO Auto-generated method stub
+		String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+		int dimesNum = Integer.parseInt(dimes);
+		int dimesTotal = dimesNum * 10;
+		return dimesTotal;
+	}
+	public static int getQuartersAmount() {
+		// TODO Auto-generated method stub
+		String quarters = JOptionPane.showInputDialog("How many quarters do you have?");
+		int quartersNum = Integer.parseInt(quarters);
+		int quartersTotal = quartersNum * 25;
+		return quartersTotal;
 	}
 }
