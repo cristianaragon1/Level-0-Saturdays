3b49b5e26ed320faa14761dc066e54b72f02b15e
Love4653 <Kaiya.park@gmail.com>
2016-06-04 09:51:44 -0700

    2 projects done
    2 projects done

---
+++ src/bottles.java
@@ -0,0 +1,11 @@
+
+public class bottles {
+	public static void main(String[] args) {
+		for (int i = 99; i > 1; i= i - 3) {
+			System.out.println(i + " bottles of water on the wall, " + i + " bottles of water," + " Take three down pass them around");
+			System.out.println("drinketey drink till the water runs out!");
+		}
+		System.out.println(1 + " bottle of water on the wall, " + 1 + " bottle of water," + " Take it down pass it around" );
+		System.out.println("drinketey drink till the water runs out!");
+	}
+}
